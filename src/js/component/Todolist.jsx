c1beb1b7c791d9dbf687ed24edb2e5d7d47e967d
Antoniomg3 <anjemuga2610@gmail.com>
2022-10-24 13:12:29 +0000

    Version final

--- src/js/component/Todolist.jsx
+++ src/js/component/Todolist.jsx
@@ -1,10 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import shortid from "shortid";
+const Todolist = () => {
+	let [tarea, setTarea] = useState({
+		label: "",
+		done: false
+	});
+	let [listaTareas, setListaTareas] = useState([]);
+	let restantes;
+	if (listaTareas.length === 0) {
+		restantes = "No hay tareas pendientes.";
+	} else {
+		restantes = `Hay ${listaTareas.length} tareas pendientes.`;
+	}
+	const obtenerValor = e => {
+		if (e.key.toLowerCase() === "enter") {
+			let valor = e.target.value;
+			console.log(valor);
+			setTarea(
+				(tarea = {
+					label: valor,
+					done: false,
+					id: shortid.generate()
+				})
+			);
+			setListaTareas([...listaTareas, tarea]);
+		}
+		setTarea("");
+	};
+	function EliminarTareas(id) {
+		const newList = listaTareas.filter(item => item.id !== id);
+		setListaTareas(newList);
+	}
+	return (
+		<div className="container">
+			<h1>Todo List</h1>
+			<div className="input-group input-group-lg">
+				<input
+					placeholder="Agrega una tarea"
+					onKeyPress={obtenerValor}
+					type="text"
+					className="form-control"
+					aria-label="Sizing example input"
+					aria-describedby="inputGroup-sizing-lg"
+				/>
+			</div>
 
-const TodoList = (props) =>{
-    return <>
-        {
-            props.tasks.map((task) => <li className="Tasks">{task.text} <button className="removeBTN" onClick={() => props.removeTask(task.id)}>X</button></li>)}
-    </>
-}
+			<ul className="list-group d-flex">
+				{listaTareas.map((item, i) => (
+					<li className="list-group-item" key={item.id}>
+						<div className="task">
+							<div className>
+								<span className="lead">{item.label}</span>
+							</div>
+							<div>
+								<button
+									className="btn btn-danger"
+									type="button"
+									onClick={() => EliminarTareas(item.id)}>
+									<i className="far fa-trash-alt"></i>
+								</button>
+							</div>
+						</div>
+					</li>
+				))}
+			</ul>
+			<strong>{restantes}</strong>
+		</div>
+	);
+};
 
-export default TodoList;
+export default Todolist;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,44 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
+
+//include images into your bundle
 import TodoList from "./Todolist.jsx";
-import InputElem from "./InputElem.jsx";
-import ButtonElem from "./ButtonElem.jsx";
 
 //create your first component
-const Home = (props) => {
-	const [taskValue, setTaskValue] = useState("");
-	const [counter, setCounter] = useState(1);
-	const [tasks, setTasks] = useState([]);
-
-	const addTask = () =>{
-		const auxTasks = [...tasks]
-		auxTasks.push({
-			id: counter,
-			text: taskValue,
-		})
-		console.log({ auxTasks })
-		setTasks(auxTasks)
-		setCounter(counter + 1)		
-		setTaskValue("")
-	}
-
-	const removeTask = (id) => {
-		const auxTasks = [...tasks]
-		const result = auxTasks.filter(((tasks) => tasks.id !== id));
-		setTasks(result);
-		console.log({ result })
-	}
-
-	
-
-
+export function Home() {
 	return (
-		<div className="text-center">
-			<h1>TODO LIST</h1>					
-			<InputElem value={taskValue} onChange={(e) => setTaskValue(e.target.value)}/>
-			<ButtonElem onClick={addTask} value="Añadir"/>
-			<TodoList tasks={tasks} removeTask={removeTask}/>
+		<div className="text-center mt-5">
+			<TodoList />
 		</div>
 	);
-};
-
-export default Home;
+}
